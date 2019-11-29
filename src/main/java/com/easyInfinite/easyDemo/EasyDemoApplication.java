0cb346c4899813f12012614827acf15ce0dcb552
chenhongxin <admin@chenyunxuan.com>
2019-11-29 16:26:45 +0800

    <x>初始提交

---
+++ src/main/java/com/easyInfinite/easyDemo/EasyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.easyInfinite.easyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyInfinite/easyDemo/EasyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyInfinite.easyDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

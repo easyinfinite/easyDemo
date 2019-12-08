11573f65bbd729b85452ddb1cda635fc95abb6f7
chenhongxin <admin@chenyunxuan.com>
2019-12-08 19:32:50 +0800

    <x>redis并存jedis

--- easyDemo-redis/src/main/java/com/redis/base/config/RedissonConfig.java
+++ easyDemo-redis/src/main/java/com/redis/base/config/RedissonConfig.java
@@ -1,12 +1,21 @@
 package com.redis.base.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 /**
  * @ClassName:RedissonConfig
@@ -22,5 +31,44 @@ public class RedissonConfig {
         // 本例子使用的是yaml格式的配置文件，读取使用Config.fromYAML，如果是Json文件，则使用Config.fromJSON
         Config config = Config.fromYAML(RedissonConfig.class.getClassLoader().getResource("redisson-config.yml"));
         return Redisson.create(config);
+    }
+
+
+    //初始化
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(new JedisConnectionFactory());
+        //key序列化方式;（不然会出现乱码;）,但是如果方法上有Long等非String类型的话，会报类型转换错误；
+        //所以在没有自己定义key生成策略的时候，以下这个代码建议不要这么写，可以不配置或者自己实现ObjectRedisSerializer
+        //或者JdkSerializationRedisSerializer序列化方式;
+        //Long类型不可以会出现异常信息
+//        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+//        redisTemplate.setValueSerializer(new GenericToStringSerializer<Long>(Long.class));
+//        redisTemplate.setValueSerializer(new GenericToStringSerializer<String>(String.class));
+//        redisTemplate.setKeySerializer(redisSerializer);
+//        redisTemplate.setHashKeySerializer(redisSerializer);
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+//        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+
+        //解决日期序列化问题
+        ObjectMapper om = new ObjectMapper();
+        om.setDateFormat(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"));
+        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer(om);
+        redisTemplate.setDefaultSerializer(genericJackson2JsonRedisSerializer);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
     }
 }

182948b470901c0f3ff1631a4b2b2d558fb854e9
chenhongxin <admin@chenyunxuan.com>
2020-04-20 18:51:17 +0800

    <+>力扣算法题

--- easyDemo-structure/src/test/java/structure/tree/TestBst.java
+++ easyDemo-structure/src/test/java/structure/tree/TestBst.java
@@ -1,9 +1,5 @@
 package structure.tree;
 
-import com.structure.tree.Bst;
-
-import java.util.Set;
-
 /**
  * @ClassName:TestStack
  * @Description 测试栈
@@ -14,16 +10,52 @@ import java.util.Set;
 public class TestBst {
 
     public static void main(String[] args) {
-        Bst<Integer> bst = new Bst<>();
-        int[] nums = {5, 3, 2, 4, 6, 8};
-
-        for (int num : nums) {
-            bst.add(num);
-        }
-//        bst.preOrder(1);
-//        System.out.println("[][][][]");
-        bst.afterOrderWithoutRecursive();
+//        Bst<Integer> bst = new Bst<>();
+//        int[] nums = {5, 3, 2, 4, 6, 8};
+//
+//        for (int num : nums) {
+//            bst.add(num);
+//        }
+////        bst.preOrder(1);
+////        System.out.println("[][][][]");
+//        bst.afterOrderWithoutRecursive();
 //        System.out.println(bst);
+
+
+        String s = "abcdefg";
+        int k = 2;
+
+//        System.out.println(reverseLeftWords(s, k));
+
+        System.out.println(numberOfSteps(8));
+    }
+
+    //数据左移
+    public static String reverseLeftWords(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.substring(n, s.length()));
+        sb.append(s.substring(0, n));
+        return sb.toString();
+    }
+
+
+    //给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
+    public static int numberOfSteps(int num) {
+        return numberOfSteps1(num, 0);
+    }
+
+    public static int numberOfSteps1(int num, int stepNum) {
+        if (num % 2 != 0) {
+            num = num - 1;
+        } else {
+            num = num / 2;
+        }
+        stepNum++;
+        if (num == 0) {
+            return stepNum;
+        } else {
+            return numberOfSteps1(num, stepNum);
+        }
     }
 
 }
